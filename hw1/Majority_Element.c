0ae929ced96681a3a35530dc836f6c3f9527d8ed
Chi-you <yoyolee789@gmail.com>
2021-10-22 22:48:05 +0800

    Change the testdata

--- hw1/Majority_Element.c
+++ hw1/Majority_Element.c
@@ -17,7 +17,7 @@ int majorityElement(int num[], int n){
 }
 
 int main(){
-    int A[] = {1, 2, 2, 5, 4, 2, 2, 1, 2, 2};
+    int A[] = {1, 2, 2, 5, 2, 2, 4, 1, 2, 2};
     int ans = majorityElement(A, 10);
     printf("The majority element is %d\n", ans);
     return 0;
